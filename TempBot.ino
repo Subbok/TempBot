9f2d30d6c99807a757f4a160d2474b27d4f00a5a
Subbok <tommkobuss@gmail.com>
2024-04-17 02:56:19 +0200

    Change and array

--- TempBot.ino
+++ TempBot.ino
@@ -1,33 +1,18 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 #include <DHT.h>
-#include <DHT_U.h>
-
-#define DHTPIN 8
-#define DHTTYPE DHT22 
+#include <Wire.h>
+#include <SPI.h>
 
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
 #define OLED_RESET -1
 #define SCREEN_ADDRESS 0x3C
-
-sensors_event_t event;
-sensors_event_t humidityEvent;
-sensor_t sensor;
-
-String temp_text = "Temp:";
-String hum_text = "Hum:";
-
-String text = "23.7%";
-
-String temp = String(event.temperature, 2);
-String hum = String(humidityEvent.relative_humidity);
+#define DHTPIN 8
+#define DHTTYPE DHT22
 
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
-
-DHT_Unified dht(DHTPIN, DHTTYPE);
-
-uint32_t delayMS;
+DHT dht(DHTPIN, DHTTYPE);
 
 void setup()
 {
@@ -44,54 +29,57 @@ void setup()
     display.display();
 
     dht.begin();
-
-    dht.temperature().getSensor(&sensor);
-    delayMS = sensor.min_delay / 1000;
 }
 
 void loop()
 {
-    delay(delayMS);
+    delay(2000);
 
-    dht.temperature().getEvent(&event);
-    if (isnan(event.temperature))
+    float h = dht.readHumidity();
+    float t = dht.readTemperature();
+
+    if (isnan(h) || isnan(t))
     {
-        Serial.println(F("Error reading temperature!"));
+        Serial.println(F("Failed to read from DHT sensor!"));
         return;
     }
 
-    dht.humidity().getEvent(&humidityEvent);
-    if (isnan(humidityEvent.relative_humidity))
-    {
-        Serial.println(F("Error reading humidity!"));
-        return;
-    }
+    float hic = dht.computeHeatIndex(t, h, false);
+
+    displayOled(t, h, hic);
+}
+
+
+void displayOled(float t, float h, float hic)
+{
+    String t_str = String(t, 1);
+    String h_str = String(h, 1);
+    String hic_str = String(hic, 1);
+
+    if (t_str.endsWith(".0")) t_str.remove(t_str.length() - 2, 2);
+    if (h_str.endsWith(".0")) h_str.remove(h_str.length() - 2, 2);
+    if (hic_str.endsWith(".0")) hic_str.remove(hic_str.length() - 2, 2);
 
     display.clearDisplay();
-
+    display.setTextColor(WHITE);
     display.setTextSize(1);
-    display.setTextColor(SSD1306_WHITE);
-
-    display.setCursor(64 - temp_text.length() * 2, 0);
-    display.print(F("Temp: "));
-    display.setCursor(64 - 12, 10);
-    display.print(event.temperature);
+    display.setCursor(5, 15);
+    display.print("Humidity:");
+    display.setCursor(80, 15);
+    display.print(h_str);
+    display.print("%");
+    display.setCursor(5, 30);
+    display.print("Temperature:");
+    display.setCursor(80, 30);
+    display.print(t_str);
+    display.print((char)247); 
+    display.print("C");
+    display.setCursor(5, 45);
+    display.print("Heat Index:");
+    display.setCursor(80, 45);
+    display.print(hic_str);
     display.print((char)247);
-    display.println("C");
-
-    display.setCursor(64 - hum_text.length() * 2, 20);
-    display.print(F("Hum: "));
-    display.setCursor(64 - 5, 30);
-    display.print(event.relative_humidity);
-    display.println(F("%"));
-
+    display.print("C");
     display.display();
-
-    Serial.print(F("Temperature: "));
-    Serial.print(event.temperature);
-    Serial.println(F("°C"));
-    Serial.print(F("Humidity: "));
-    Serial.print(humidityEvent.relative_humidity);
-    Serial.println(F("%"));
-    Serial.print(text.length());
+    
 }
